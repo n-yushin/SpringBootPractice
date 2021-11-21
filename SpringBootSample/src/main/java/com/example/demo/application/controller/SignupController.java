24d4f21aff2fa535dca80306d36786863c142f0d
n-yushin <gaiabass0215nomura@gmail.com>
2021-11-21 13:47:02 +0900

    多言語対応まで

--- SpringBootSample/src/main/java/com/example/demo/application/controller/SignupController.java
+++ SpringBootSample/src/main/java/com/example/demo/application/controller/SignupController.java
@@ -1,5 +1,6 @@
 package com.example.demo.application.controller;
 
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.application.service.UserApplicationService;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 @Controller
 @RequestMapping("/user")
 public class SignupController {
@@ -20,9 +23,9 @@ public class SignupController {
 	
 	/** ユーザ登録画面を表示 */
 	@GetMapping("/signup")
-	public String getSignUp(Model model) {
+	public String getSignUp(Model model, Locale locale) {
 		// 性別を登録
-		Map<String,Integer> genderMap = userApplicationService.getGenderMap();
+		Map<String,Integer> genderMap = userApplicationService.getGenderMap(locale);
 		model.addAttribute("genderMap", genderMap);
 		
 		// ユーザ登録画面に遷移
