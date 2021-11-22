4b884b387e66502d886fd7f3f1d7e354c51370e5
n-yushin <gaiabass0215nomura@gmail.com>
2021-11-22 14:33:20 +0900

    アノテーションによるバリーデーションの追加およびバリデータメッセージの設定

--- SpringBootSample/src/main/java/com/example/demo/application/controller/SignupController.java
+++ SpringBootSample/src/main/java/com/example/demo/application/controller/SignupController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,7 +38,7 @@ public class SignupController {
 	}
 	
 	@PostMapping("/signup")
-	public String posetSignUp(Model model, Locale locale, @ModelAttribute SignupForm form, BindingResult bindingResult) {
+	public String posetSignUp(Model model, Locale locale, @ModelAttribute @Validated SignupForm form, BindingResult bindingResult) {
 		// 入力チェック
 		if(bindingResult.hasErrors()) {
 			// 入力エラーがあった場合、ユーザー情報登録画面に戻る
--- SpringBootSample/src/main/java/com/example/demo/application/form/SignupForm.java
+++ SpringBootSample/src/main/java/com/example/demo/application/form/SignupForm.java
@@ -2,21 +2,42 @@ package com.example.demo.application.form;
 
 import java.util.Date;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
 @Data
 public class SignupForm {
+	@NotBlank
+	@Email
 	private String userId;
+	
+	@NotBlank
+	@Length(min=4, max=100)
+	@Pattern(regexp="^[a-zA-z0-9]+$")
 	private String password;
+	
+	@NotBlank
 	private String userName;
 	
 	// 指定のフォーマットで入力を受け取ることで、入力値をDate型にバインド可能
 	@DateTimeFormat(pattern="yyyy/MM/dd")
+	@NotNull
 	private Date birthday;
 	
+	@Min(20)
+	@Max(100)
 	private int age;
+	
+	@NotNull
 	private int gender;
 	
 }
